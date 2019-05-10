ebc070cecd7ed910d437475138292a9eaac1d3eb
J. Mark Stevens <github@jmarkstevens.com>
2019-05-10 09:09:30 -0700

    basic server setup

---
+++ server/webpack.common.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        exclude: [path.resolve(__dirname, 'node_modules')],
+        test: /\.ts$/,
+        use: 'ts-loader'
+      },
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto'
+      }
+    ]
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.mjs', '.ts', '.js']
+  },
+  target: 'node'
+}
