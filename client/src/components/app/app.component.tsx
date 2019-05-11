efecc12ecce0b948e51b94cffb5bdc13aec25b4b
J. Mark Stevens <github@jmarkstevens.com>
2019-05-11 14:36:07 -0700

    open in chrome

--- client/src/components/app/app.component.tsx
+++ client/src/components/app/app.component.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 interface Props {}
 
 const AppComponent: React.FunctionComponent<Props> = () => {
-  return <div className="app-container">AppComponent</div>
+  return <div className="app-container">AppComponent loaded</div>
 }
 
 export default AppComponent
