15876ae6140c100c6355bb45e78109d162897c1c
J. Mark Stevens <github@jmarkstevens.com>
2019-05-11 10:44:42 -0700

    basic client setup

---
+++ client/src/app.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+const title = 'My Minimal Typescript Webpack Setup'
+
+ReactDOM.render(<div>{title}</div>, document.getElementById('react-app'))
---
+++ client/wp.config.js
@@ -0,0 +1,37 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const ROOT_PATH = path.resolve(__dirname)
+const DIST_PATH = path.resolve(ROOT_PATH, './dist')
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    app: './src/app.tsx'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
+    ]
+  },
+  output: {
+    path: DIST_PATH,
+    filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  resolve: {
+    extensions: ['*', '.css', '.js', '.scss', '.ts', '.tsx']
+  }
+}
